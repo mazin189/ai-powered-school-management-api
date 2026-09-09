ad2402f23097fd8c87677a24577a8cac21bb97f2
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-09-10 00:01:45 +0300

    removing variables for development only not production

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -1,8 +1,6 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest(
-{ 
-id: "sms-lms",
-isDev: true 
+export const inngest = new Inngest({
+  id: "sms-lms",
 });
